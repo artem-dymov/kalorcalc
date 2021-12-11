a3c5050caf9cc9876a016469736fa50f72834250
Artem Dymov <dymov.artem@outlook.com>
2021-12-11 19:58:53 +0200

    2

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
-root = tk.Tk()
-
-tk.Button(text="Hello").pack()
-ttk.Button(text="Hello").pack()
-
-tk.Checkbutton(text="Hello").pack()
-ttk.Checkbutton(text="Hello").pack()
-
-tk.Radiobutton(text="Hello").pack()
-ttk.Radiobutton(text="Hellooog").pack()
-
-root.mainloop()
