dade1f1ffb303b1cbceb0608141fca0bedd3f8e7
Artem Dymov <dymov.artem@outlook.com>
2021-12-12 14:06:30 +0200

    first login window

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
-import tkinter as tk
-import tkinter.ttk as ttk
+import tkinter
+from tkinter import *
+from tkinter import ttk
 
-print("hello world")
-print("Hello kpi")
+
+class LoginPage:
+    window = Tk()
+    window.title("Login")
+    window.geometry("300x200")
+
+    frame = ttk.Frame(window, padding=10)
+
+    Label(text="Вітаємо у KalorCalc!").pack()
+    LabelName = tkinter.Label(window, text="Username:")
+    LabelName.pack()
+
+    getusername = tkinter.StringVar()
+    getpassword = tkinter.StringVar()
+
+    TxtBoxName = tkinter.Entry(window, textvariable=getusername)
+    TxtBoxName.pack()
+
+
+    Label(window, text="Password:").pack()
+    Entry(window, textvariable=getpassword).pack()
+
+
+    Button(window, text="Enter").pack()
+
+    window.mainloop()
+
+
+
+if __name__ == "__main__":
+    LoginPage
