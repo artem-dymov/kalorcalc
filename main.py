0dac244ddec10e93ed1fb5d5eb125b752019b746
Artem Dymov <dymov.artem@outlook.com>
2021-12-27 05:26:04 +0200

    final auth, final params, menu in progress

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pylib import pycsv, calculate
 
 class LoginPage:
     def __init__(self, root):
-        window = Toplevel(root)
+        window = tk.Toplevel(root)
         window.title("Login")
         window.geometry("300x200")
 
@@ -33,10 +33,12 @@ class LoginPage:
         if username_entry.get() == "" or password_entry.get() == "":
             ErrorEmptyPage(root)
 
-        elif username_entry.get() in pycsv.get_usernames():
-            if pycsv.check_password(username_entry.get(), password_entry.get()) == True:
+        elif username_entry.get().lower() in pycsv.get_usernames():
+            if username_entry.get().lower() == 'username':
+                ErrorWrongPasswordPage(root)
+            elif pycsv.check_password(username_entry.get(), password_entry.get()) == True:
                 window.withdraw()
-                MenuPage(root)
+                MenuPage(root, username_entry.get())
             else:
                 ErrorWrongPasswordPage(root)
 
@@ -44,12 +46,11 @@ class LoginPage:
         else:
             pycsv.auth_writer(username_entry.get(), password_entry.get())
             window.withdraw()
-            ParametersPage(root)
-
+            ParametersPage(root, username_entry.get())
 
 class ParametersPage:
-    def __init__(self, root):
-        window = Toplevel(root)
+    def __init__(self, root, username):
+        window = tk.Toplevel(root)
         window.title("Parameters")
         window.geometry('500x500')
 
@@ -72,55 +73,68 @@ class ParametersPage:
 
         ttk.Label(window, text="Ваша стать").pack()
 
-        var_sex = tk.IntVar()
-        var_sex.set(1)
+        var_sex = tk.StringVar()
+        var_sex.set("male")
 
-        r_btn_male = ttk.Radiobutton(window, text="Чоловіча", variable=var_sex, value=1)
+        r_btn_male = ttk.Radiobutton(window, text="Чоловіча", variable=var_sex, value="male")
         r_btn_male.pack()
 
-        r_btn_female = ttk.Radiobutton(window, text="Жіноча", variable=var_sex, value=2)
+        r_btn_female = ttk.Radiobutton(window, text="Жіноча", variable=var_sex, value="female")
         r_btn_female.pack()
 
         ttk.Label(window, text="Рівень вашої активності").pack()
 
-        var_activity = tk.IntVar()
-        var_activity.set(1)
+        var_activity = tk.StringVar()
+        var_activity.set("low")
 
-        r_btn_activity_low = ttk.Radiobutton(window, variable=var_activity, value=1, text="Низький")
+        r_btn_activity_low = ttk.Radiobutton(window, variable=var_activity, value="low", text="Низький")
         r_btn_activity_low.pack()
 
-        r_btn_activity_pomirn = ttk.Radiobutton(window, variable=var_activity, value=2, text="Помірний")
+        r_btn_activity_pomirn = ttk.Radiobutton(window, variable=var_activity, value="pomirn", text="Помірний")
         r_btn_activity_pomirn.pack()
 
-        r_btn_activity_middle = ttk.Radiobutton(window, variable=var_activity, value=3, text="Середній")
+        r_btn_activity_middle = ttk.Radiobutton(window, variable=var_activity, value="middle", text="Середній")
         r_btn_activity_middle.pack()
 
-        r_btn_activity_high = ttk.Radiobutton(window, variable=var_activity, value=4, text="Високий")
+        r_btn_activity_high = ttk.Radiobutton(window, variable=var_activity, value="high", text="Високий")
         r_btn_activity_high.pack()
 
-        r_btn_activity_very_high = ttk.Radiobutton(window, variable=var_activity, value=5, text="Дуже високий")
+        r_btn_activity_very_high = ttk.Radiobutton(window, variable=var_activity, value="very_high", text="Дуже високий")
         r_btn_activity_very_high.pack()
 
         ttk.Label(window, text="Чого ви хочете?").pack()
 
-        var_want = tk.IntVar()
-        var_want.set(1)
+        var_want = tk.StringVar()
+        var_want.set("lose")
 
-        r_btn_want_lose = ttk.Radiobutton(window, variable=var_want, value=1, text="Схуднути")
+        r_btn_want_lose = ttk.Radiobutton(window, variable=var_want, value="lose", text="Схуднути")
         r_btn_want_lose.pack()
 
-        r_btn_want_not = ttk.Radiobutton(window, variable=var_want, value=2, text="Не хочу міняти вагу")
+        r_btn_want_not = ttk.Radiobutton(window, variable=var_want, value="like", text="Не хочу міняти вагу")
         r_btn_want_not.pack()
 
-        r_btn_want_gain = ttk.Radiobutton(window, variable=var_want, value=3, text="Хочу набрати вагу")
+        r_btn_want_gain = ttk.Radiobutton(window, variable=var_want, value="gaine", text="Хочу набрати вагу")
         r_btn_want_gain.pack()
+
+
+        ttk.Button(window, text="Enter", command=lambda: self.enter_params(
+            username, entry_mass.get(), entry_growth.get(), entry_age.get(),
+            var_sex.get(), var_activity.get(), var_want.get(), root, window
+        )).pack()
 
         ttk.Button(window, text="Quit", command=lambda: window.quit()).pack()
 
+    def enter_params(self, username, mass, growth, age, sex, activity, want, root, window):
+        if mass == "" or growth == "" or age == "":
+            ErrorEmptyPage(root)
+        else:
+            pycsv.write_params(username, mass, growth, age, sex, activity, want)
+            window.withdraw()
+            MenuPage(root, username)
 
 class ErrorEmptyPage:
     def __init__(self, root):
-        self.window = Toplevel(root)
+        self.window = tk.Toplevel(root)
         self.window.title("Помилка!")
         self.window.geometry('200x125')
 
@@ -131,7 +145,7 @@ class ErrorEmptyPage:
 
 class ErrorWrongPasswordPage:
     def __init__(self, root):
-        self.window = Toplevel(root)
+        self.window = tk.Toplevel(root)
         self.window.title("Помилка!")
         self.window.geometry("225x150")
 
@@ -141,12 +155,21 @@ class ErrorWrongPasswordPage:
 
 
 class MenuPage:
-    def __init__(self, root):
-        self.window = Toplevel(root)
+    def __init__(self, root, username):
+        self.window = tk.Toplevel(root)
         self.window.title("Меню")
         self.window.geometry('400x300')
 
         frame = ttk.Frame(self.window, padding=10)
+
+
+        mass_growth = pycsv.get_db_mass_growth(username)
+
+        tk.Label(self.window, text=calculate.imt_category(calculate.imt(mass_growth[0], mass_growth[1]))).pack()
+
+
+
+
 
 
 def main():
@@ -158,3 +181,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
